7056d000172e3766d7b8ad32b86a4913d97e36cc
Stephanos Constantinou <35262217+SConstantinou@users.noreply.github.com>
2018-09-27 17:06:08 +0300

    In Progress

--- SysInfo/Get-HeatPipe.ps1
+++ SysInfo/Get-HeatPipe.ps1
@@ -1,3 +1,90 @@
 ﻿function Get-HeatPipe {
-    Get-CimInstance -ClassName Win32_HeatPipe -Property *
+    
+    $Availability = @{
+        '1' = 'Other'
+        '2' = 'Unknown'
+        '3' = 'Running or Full Power'
+        '4' = 'Warning'
+        '5' = 'In Test'
+        '6' = 'Not Applicable'
+        '7' = 'Power Off'
+        '8' = 'Off Line'
+        '9' = 'Off Duty'
+        '10' = 'Degraded'
+        '11' = 'Not Installed'
+        '12' = 'Install Error'
+        '13' = 'Power Save - Unknown'
+        '14' = 'Power Save - Low Power Mode'
+        '15' = 'Power Save - Standby'
+        '16' = 'Power Cycle'
+        '17' = 'Power Save - Warning'
+        '18' = 'Pause'
+        '19' = 'Not Ready'
+        '20' = 'Not Configured'
+        '21' = 'Quiesced - Device is Quiet'
+    }
+
+    $ConfigManagerErrorCode = @{
+        '0' = 'This device is working properly.'
+        '1' = 'This device is not configured correctly.'
+        '2' = 'Windows cannot load the driver for this device.'
+        '3' = 'The driver for this device might be corrupted, or your system may be running low on memory or other resources.'
+        '4' = 'This device is not working properly. One of its drivers or your registry might be corrupted.'
+        '5' = 'The driver for this device needs a resource that Windows cannot manage.'
+        '6' = 'The boot configuration for this device conflicts with other devices.'
+        '7' = 'Cannot filter.'
+        '8' = 'The driver loader for the device is missing.'
+        '9' = 'This device is not working properly because the controlling firmware is reporting the resources for the device incorrectly.'
+        '10' = 'This device cannot start.'
+        '11' = 'This device failed.'
+        '12' = 'This device cannot find enough free resources that it can use.'
+        '13' = "Windows cannot verify this device's resources."
+        '14' = 'This device cannot work properly until you restart your computer.'
+        '15' = 'This device is not working properly because there is probably a re-enumeration problem.'
+        '16' = 'Windows cannot identify all the resources this device uses.'
+        '17' = 'This device is asking for an unknown resource type.'
+        '18' = 'Reinstall the drivers for this device.'
+        '19' = 'Failure using the VxD loader.'
+        '20' = 'Your registry might be corrupted.'
+        '21' = 'System failure: Try changing the driver for this device. If that does not work, see your hardware documentation. Windows is removing this device.'
+        '22' = 'This device is disabled.'
+        '23' = "System failure: Try changing the driver for this device. If that doesn't work, see your hardware documentation."
+        '24' = 'This device is not present, is not working properly, or does not have all its drivers installed.'
+        '25' = 'Windows is still setting up this device.'
+        '26' = 'Windows is still setting up this device.'
+        '27' = 'This device does not have valid log configuration.'
+        '28' = 'The drivers for this device are not installed.'
+        '29' = 'This device is disabled because the firmware of the device did not give it the required resources.'
+        '30' = 'This device is using an Interrupt Request (IRQ) resource that another device is using.'
+        '31' = 'This device is not working properly because Windows cannot load the drivers required for this device.'
+    }
+
+    $PowerManagementCapabilities = @{
+        '0' = 'Unknown'
+        '1' = 'Not Supported'
+        '2' = 'Disabled'
+        '3' = 'Enabled'
+        '4' = 'Power Saving Modes Entered Automatically'
+        '5' = 'Power State Settable'
+        '6' = 'Power Cycling Supported'
+        '7' = 'Timed Power On Supported'
+    }
+
+    $StatusInfo = @{
+        '1' = 'Other'
+        '2' = 'Unknown'
+        '3' = 'Enabled'
+        '4' = 'Disabled'
+        '5' = 'Not Applicable'
+    }
+    
+    $Properties = 'Name','Description','Caption','Availability','ActiveCooling','ConfigManagerErrorCode',
+                  'ConfigManagerUserConfig','PowerManagementSupported','PowerManagementCapabilities',
+                  'StatusInfo','Status'
+
+    $HeatPipe = Get-CimInstance -ClassName Win32_HeatPipe -Property $Properties | Select-Object -Property $Properties
+    $HeatPipe | ForEach-Object {$_.Availability = $Availability["$($_.Availability)"]}
+    $HeatPipe | ForEach-Object {$_.ConfigManagerErrorCode = $ConfigManagerErrorCode["$($_.ConfigManagerErrorCode)"]}
+    $HeatPipe | ForEach-Object {$_.PowerManagementCapabilities = $PowerManagementCapabilities["$($_.PowerManagementCapabilities)"]}
+    $HeatPipe | ForEach-Object {$_.StatusInfo = $StatusInfo["$($_.StatusInfo)"]}
 }
--- SysInfo/Get-Keyboard.ps1
+++ SysInfo/Get-Keyboard.ps1
@@ -1,6 +1,84 @@
 ﻿function Get-Keyboard {
 
-    $KeyboardLayout = @{
+    $Availability = @{
+        '1' = 'Other'
+        '2' = 'Unknown'
+        '3' = 'Running or Full Power'
+        '4' = 'Warning'
+        '5' = 'In Test'
+        '6' = 'Not Applicable'
+        '7' = 'Power Off'
+        '8' = 'Off Line'
+        '9' = 'Off Duty'
+        '10' = 'Degraded'
+        '11' = 'Not Installed'
+        '12' = 'Install Error'
+        '13' = 'Power Save - Unknown'
+        '14' = 'Power Save - Low Power Mode'
+        '15' = 'Power Save - Standby'
+        '16' = 'Power Cycle'
+        '17' = 'Power Save - Warning'
+        '18' = 'Pause'
+        '19' = 'Not Ready'
+        '20' = 'Not Configured'
+        '21' = 'Quiesced - Device is Quiet'
+    }
+
+    $ConfigManagerErrorCode = @{
+        '0' = 'This device is working properly.'
+        '1' = 'This device is not configured correctly.'
+        '2' = 'Windows cannot load the driver for this device.'
+        '3' = 'The driver for this device might be corrupted, or your system may be running low on memory or other resources.'
+        '4' = 'This device is not working properly. One of its drivers or your registry might be corrupted.'
+        '5' = 'The driver for this device needs a resource that Windows cannot manage.'
+        '6' = 'The boot configuration for this device conflicts with other devices.'
+        '7' = 'Cannot filter.'
+        '8' = 'The driver loader for the device is missing.'
+        '9' = 'This device is not working properly because the controlling firmware is reporting the resources for the device incorrectly.'
+        '10' = 'This device cannot start.'
+        '11' = 'This device failed.'
+        '12' = 'This device cannot find enough free resources that it can use.'
+        '13' = "Windows cannot verify this device's resources."
+        '14' = 'This device cannot work properly until you restart your computer.'
+        '15' = 'This device is not working properly because there is probably a re-enumeration problem.'
+        '16' = 'Windows cannot identify all the resources this device uses.'
+        '17' = 'This device is asking for an unknown resource type.'
+        '18' = 'Reinstall the drivers for this device.'
+        '19' = 'Failure using the VxD loader.'
+        '20' = 'Your registry might be corrupted.'
+        '21' = 'System failure: Try changing the driver for this device. If that does not work, see your hardware documentation. Windows is removing this device.'
+        '22' = 'This device is disabled.'
+        '23' = "System failure: Try changing the driver for this device. If that doesn't work, see your hardware documentation."
+        '24' = 'This device is not present, is not working properly, or does not have all its drivers installed.'
+        '25' = 'Windows is still setting up this device.'
+        '26' = 'Windows is still setting up this device.'
+        '27' = 'This device does not have valid log configuration.'
+        '28' = 'The drivers for this device are not installed.'
+        '29' = 'This device is disabled because the firmware of the device did not give it the required resources.'
+        '30' = 'This device is using an Interrupt Request (IRQ) resource that another device is using.'
+        '31' = 'This device is not working properly because Windows cannot load the drivers required for this device.'
+    }
+
+    $PowerManagementCapabilities = @{
+        '0' = 'Unknown'
+        '1' = 'Not Supported'
+        '2' = 'Disabled'
+        '3' = 'Enabled'
+        '4' = 'Power Saving Modes Entered Automatically'
+        '5' = 'Power State Settable'
+        '6' = 'Power Cycling Supported'
+        '7' = 'Timed Power On Supported'
+    }
+
+    $StatusInfo = @{
+        '1' = 'Other'
+        '2' = 'Unknown'
+        '3' = 'Enabled'
+        '4' = 'Disabled'
+        '5' = 'Not Applicable'
+    }
+    
+    $Layout = @{
         '0000041c' = 'Albanian'
         '00000401' = 'Arabic (101)'
         '00010401' = 'Arabic (102)'
@@ -203,12 +281,16 @@
         '0000046a' = 'Yoruba'
     }
 
-    $Properties = 'Name','Description','Caption','PowerManagementCapabilities',
-                  'PowerManagementSupported','Layout','NumberOfFunctionKeys',
-                  'IsLocked','Status'
+    $Properties = 'Name','Description','Caption','Availability','PowerManagementCapabilities',
+                  'PowerManagementSupported','ConfigManagerErrorCode','ConfigManagerUserConfig',
+                  'Password','Layout','NumberOfFunctionKeys','IsLocked','StatusInfo','Status'
 
     $Keyboard = Get-CimInstance -ClassName Win32_Keyboard -Property $Properties | Select-Object $Properties
-    $Keyboard | ForEach-Object {$_.Layout = $KeyboardLayout["$($_.Layout)"]}
+    $Keyboard | ForEach-Object {$_.Layout = $Layout["$($_.Layout)"]}
+    $Keyboard | ForEach-Object {$_.Availability = $Availability["$($_.Availability)"]}
+    $Keyboard | ForEach-Object {$_.ConfigManagerErrorCode = $ConfigManagerErrorCode["$($_.ConfigManagerErrorCode)"]}
+    $Keyboard | ForEach-Object {$_.PowerManagementCapabilities = $PowerManagementCapabilities["$($_.PowerManagementCapabilities)"]}
+    $Keyboard | ForEach-Object {$_.StatusInfo = $StatusInfo["$($_.StatusInfo)"]}
     
     Write-Output $Keyboard
 }
