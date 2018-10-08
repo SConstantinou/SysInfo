0fb14e8adc5aeac49a19ba26218a0a96ca6b2991
Stephanos Constantinou <35262217+SConstantinou@users.noreply.github.com>
2018-10-08 18:00:56 +0300

    Initial Commit

---
+++ SysInfo/Public/Get-LogicalDiskRootDirectory.ps1
@@ -0,0 +1,20 @@
+﻿function Get-LogicalDiskRootDirectory {
+
+    [cmdletbinding()]
+
+    param (
+        [parameter(ValueFromPipeline = $true)][alias("cn")][String[]]$ComputerName)
+
+    $Properties = ((Get-CimClass -ClassName Win32_LogicalDiskRootDirectory).CimClassProperties).Name
+
+    if ($ComputerName -eq ''){
+
+        $LogicalDiskRootDirectory = Get-CimInstance -ClassName Win32_LogicalDiskRootDirectory -Property $Properties | Select-Object $Properties
+    }
+    else{
+
+        $LogicalDiskRootDirectory = Get-CimInstance -ClassName Win32_LogicalDiskRootDirectory -Property $Properties -ComputerName $ComputerName | Select-Object $Properties
+    }
+    
+    Write-Output $COMClass
+}
---
+++ SysInfo/Public/Get-MappedLogicalDisk.ps1
@@ -0,0 +1,29 @@
+﻿function Get-MappedLogicalDisk {
+
+    [cmdletbinding()]
+
+    param (
+        [parameter(ValueFromPipeline = $true)][alias("cn")][String[]]$ComputerName)
+
+    $Properties = ((Get-CimClass -ClassName Win32_MappedLogicalDisk).CimClassProperties).Name
+
+    if ($ComputerName -eq ''){
+
+        $MappedLogicalDisk = Get-CimInstance -ClassName Win32_MappedLogicalDisk -Property $Properties | Select-Object $Properties
+    }
+    else{
+
+        $MappedLogicalDisk = Get-CimInstance -ClassName Win32_MappedLogicalDisk -Property $Properties -ComputerName $ComputerName | Select-Object $Properties
+    }
+
+    foreach ($_ in $MappedLogicalDisk){
+
+        $_.Access = Get-Access ($_.Access)
+        $_.Availability = Get-Availability ($_.Availability)
+        $_.ConfigManagerErrorCode = Get-ConfigManagerErrorCode ($_.ConfigManagerErrorCode)
+        $_.PowerManagementCapabilities = Get-PowerManagementCapabilities ($_.PowerManagementCapabilities)
+        $_.StatusInfo = Get-StatusInfo ($_.StatusInfo)
+    }
+    
+    Write-Output $MappedLogicalDisk
+}
