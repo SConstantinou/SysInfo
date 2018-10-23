afc911cdebc6d6357e24cd854ed713dc0695f399
Stephanos Constantinou <35262217+SConstantinou@users.noreply.github.com>
2018-10-23 08:39:42 +0300

    Added Help Info

--- SysInfo/Public/Get-FloppyController.ps1
+++ SysInfo/Public/Get-FloppyController.ps1
@@ -1,4 +1,84 @@
 ﻿function Get-FloppyController {
+<#
+.SYNOPSIS
+
+Gets the capabilities and management capacity of a floppy
+disk drive controller.
+
+.DESCRIPTION
+
+Gets the capabilities and management capacity of a floppy
+disk drive controller and converts all codes in results into
+human readable format. Get-FloppyController is not available
+for use as of Windows 10 and Windows Server 2016.
+
+.PARAMETER ComputerName
+
+Specifies the computer names or IP Addresses of the systems that
+we want to get the information from.
+
+.INPUTS
+
+System.Array. Get-FloppyController can accept a string value to
+determine the ComputerName parameter.
+
+.OUTPUTS
+
+System.Object. Get-FloppyController returns an object containing
+all the information that has been retrieved.
+
+.EXAMPLE
+
+PS C:\> Get-FloppyController
+
+.EXAMPLE
+
+PS C:\> Get-FloppyController -ComputerName Server1
+
+.EXAMPLE
+
+PS C:\> Get-FloppyController -ComputerName "192.168.0.5"
+
+.EXAMPLE
+
+PS C:\> Get-FloppyController -ComputerName Server1,Server2,Server3
+
+.EXAMPLE
+
+PS C:\> Get-FloppyController -ComputerName "192.168.0.5","192.168.0.6","192.168.0.7"
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> Get-FloppyController -ComputerName $MyServers
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> Get-FloppyController -ComputerName $MyIPs
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> $MyServers | Get-FloppyController
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> $MyIPs | Get-FloppyController
+
+.EXAMPLE
+
+PS C:\> "Server1" | Get-FloppyController
+
+.EXAMPLE
+
+PS C:\> "192.168.0.5" | Get-FloppyController
+
+.LINK
+
+https://www.sconstantinou.com/get-floppycontroller
+#>
 
     [cmdletbinding()]
 
--- SysInfo/Public/Get-FloppyDrive.ps1
+++ SysInfo/Public/Get-FloppyDrive.ps1
@@ -1,4 +1,83 @@
 ﻿function Get-FloppyDrive {
+<#
+.SYNOPSIS
+
+Gets the information and functions of a floppy disk drive.
+
+.DESCRIPTION
+
+Gets the information and functions of a floppy disk drive
+and converts all codes in results into human readable format.
+Get-FloppyDrive is not available for use as of Windows 10
+and Windows Server 2016.
+
+.PARAMETER ComputerName
+
+Specifies the computer names or IP Addresses of the systems that
+we want to get the information from.
+
+.INPUTS
+
+System.Array. Get-FloppyDrive can accept a string value to
+determine the ComputerName parameter.
+
+.OUTPUTS
+
+System.Object. Get-FloppyDrive returns an object containing
+all the information that has been retrieved.
+
+.EXAMPLE
+
+PS C:\> Get-FloppyDrive
+
+.EXAMPLE
+
+PS C:\> Get-FloppyDrive -ComputerName Server1
+
+.EXAMPLE
+
+PS C:\> Get-FloppyDrive -ComputerName "192.168.0.5"
+
+.EXAMPLE
+
+PS C:\> Get-FloppyDrive -ComputerName Server1,Server2,Server3
+
+.EXAMPLE
+
+PS C:\> Get-FloppyDrive -ComputerName "192.168.0.5","192.168.0.6","192.168.0.7"
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> Get-FloppyDrive -ComputerName $MyServers
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> Get-FloppyDrive -ComputerName $MyIPs
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> $MyServers | Get-FloppyDrive
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> $MyIPs | Get-FloppyDrive
+
+.EXAMPLE
+
+PS C:\> "Server1" | Get-FloppyDrive
+
+.EXAMPLE
+
+PS C:\> "192.168.0.5" | Get-FloppyDrive
+
+.LINK
+
+https://www.sconstantinou.com/get-floppydrive
+#>
 
     [cmdletbinding()]
 
