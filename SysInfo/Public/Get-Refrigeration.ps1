cb1e42116810d68c6241e70cb72f2cd51b94ecc9
Stephanos Constantinou <35262217+SConstantinou@users.noreply.github.com>
2018-10-24 13:16:38 +0300

    Added Help Info

--- SysInfo/Public/Get-Refrigeration.ps1
+++ SysInfo/Public/Get-Refrigeration.ps1
@@ -1,4 +1,81 @@
 ﻿function Get-Refrigeration {
+<#
+.SYNOPSIS
+
+Gets the properties of a refrigeration device.
+
+.DESCRIPTION
+
+Gets the properties of a refrigeration device and converts
+all codes in results into human readable format.
+
+.PARAMETER ComputerName
+
+Specifies the computer names or IP Addresses of the systems that
+we want to get the information from.
+
+.INPUTS
+
+System.Array. Get-Refrigeration can accept a string value to
+determine the ComputerName parameter.
+
+.OUTPUTS
+
+System.Object. Get-Refrigeration returns an object containing
+all the information that has been retrieved.
+
+.EXAMPLE
+
+PS C:\> Get-Refrigeration
+
+.EXAMPLE
+
+PS C:\> Get-Refrigeration -ComputerName Server1
+
+.EXAMPLE
+
+PS C:\> Get-Refrigeration -ComputerName "192.168.0.5"
+
+.EXAMPLE
+
+PS C:\> Get-Refrigeration -ComputerName Server1,Server2,Server3
+
+.EXAMPLE
+
+PS C:\> Get-Refrigeration -ComputerName "192.168.0.5","192.168.0.6","192.168.0.7"
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> Get-Refrigeration -ComputerName $MyServers
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> Get-Refrigeration -ComputerName $MyIPs
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> $MyServers | Get-Refrigeration
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> $MyIPs | Get-Refrigeration
+
+.EXAMPLE
+
+PS C:\> "Server1" | Get-Refrigeration
+
+.EXAMPLE
+
+PS C:\> "192.168.0.5" | Get-Refrigeration
+
+.LINK
+
+https://www.sconstantinou.com/get-refrigeration
+#>
 
     [cmdletbinding()]
 
--- SysInfo/Public/Get-RemovableDisk.ps1
+++ SysInfo/Public/Get-RemovableDisk.ps1
@@ -1,4 +1,83 @@
 ﻿function Get-RemovableDisk {
+<#
+.SYNOPSIS
+
+Gets the information of a removable disk on a computer running
+on a Windows operating system.
+
+.DESCRIPTION
+
+Gets the information of a removable disk on a computer running
+on a Windows operating system and converts all codes in results
+into human readable format.
+
+.PARAMETER ComputerName
+
+Specifies the computer names or IP Addresses of the systems that
+we want to get the information from.
+
+.INPUTS
+
+System.Array. Get-RemovableDisk can accept a string value to
+determine the ComputerName parameter.
+
+.OUTPUTS
+
+System.Object. Get-RemovableDisk returns an object containing
+all the information that has been retrieved.
+
+.EXAMPLE
+
+PS C:\> Get-RemovableDisk
+
+.EXAMPLE
+
+PS C:\> Get-RemovableDisk -ComputerName Server1
+
+.EXAMPLE
+
+PS C:\> Get-RemovableDisk -ComputerName "192.168.0.5"
+
+.EXAMPLE
+
+PS C:\> Get-RemovableDisk -ComputerName Server1,Server2,Server3
+
+.EXAMPLE
+
+PS C:\> Get-RemovableDisk -ComputerName "192.168.0.5","192.168.0.6","192.168.0.7"
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> Get-RemovableDisk -ComputerName $MyServers
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> Get-RemovableDisk -ComputerName $MyIPs
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> $MyServers | Get-RemovableDisk
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> $MyIPs | Get-RemovableDisk
+
+.EXAMPLE
+
+PS C:\> "Server1" | Get-RemovableDisk
+
+.EXAMPLE
+
+PS C:\> "192.168.0.5" | Get-RemovableDisk
+
+.LINK
+
+https://www.sconstantinou.com/get-removabledisk
+#>
 
     [cmdletbinding()]
 
