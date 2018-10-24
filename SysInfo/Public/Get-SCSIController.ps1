07481cb0a7ea43155a7c03a8424ae06b5133783f
Stephanos Constantinou <35262217+SConstantinou@users.noreply.github.com>
2018-10-24 13:17:12 +0300

    Added Help Info and bug fix

--- SysInfo/Public/Get-SCSIController.ps1
+++ SysInfo/Public/Get-SCSIController.ps1
@@ -1,4 +1,83 @@
 ﻿function Get-SCSIController {
+<#
+.SYNOPSIS
+
+Gets the information of a SCSI controller on a computer
+system running Windows.
+
+.DESCRIPTION
+
+Gets the information of a SCSI controller on a computer
+system running Windows and converts all codes in results
+into human readable format.
+
+.PARAMETER ComputerName
+
+Specifies the computer names or IP Addresses of the systems that
+we want to get the information from.
+
+.INPUTS
+
+System.Array. Get-SCSIController can accept a string value to
+determine the ComputerName parameter.
+
+.OUTPUTS
+
+System.Object. Get-SCSIController returns an object containing
+all the information that has been retrieved.
+
+.EXAMPLE
+
+PS C:\> Get-SCSIController
+
+.EXAMPLE
+
+PS C:\> Get-SCSIController -ComputerName Server1
+
+.EXAMPLE
+
+PS C:\> Get-SCSIController -ComputerName "192.168.0.5"
+
+.EXAMPLE
+
+PS C:\> Get-SCSIController -ComputerName Server1,Server2,Server3
+
+.EXAMPLE
+
+PS C:\> Get-SCSIController -ComputerName "192.168.0.5","192.168.0.6","192.168.0.7"
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> Get-SCSIController -ComputerName $MyServers
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> Get-SCSIController -ComputerName $MyIPs
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> $MyServers | Get-SCSIController
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> $MyIPs | Get-SCSIController
+
+.EXAMPLE
+
+PS C:\> "Server1" | Get-SCSIController
+
+.EXAMPLE
+
+PS C:\> "192.168.0.5" | Get-SCSIController
+
+.LINK
+
+https://www.sconstantinou.com/get-scsicontroller
+#>
 
     [cmdletbinding()]
 
@@ -22,7 +101,7 @@
 
         $_.Availability = Get-Availability ($_.Availability)
         $_.ConfigManagerErrorCode = Get-ConfigManagerErrorCode ($_.ConfigManagerErrorCode)
-        $_.PowerManagementCapabilities = Get-PowerManagementCapabilities ($_.PowerManagementCapabilities)
+        $_.PowerManagementCapabilities = Get-PowerManagementCapabilitiesCode ($_.PowerManagementCapabilities)
         $_.ProtectionManagement = Get-ProtectionManagement ($_.ProtectionManagement)
         $_.ProtocolSupported = Get-ProtocolSupported ($_.ProtocolSupported)
         $_.StatusInfo = Get-StatusInfo ($_.StatusInfo)
