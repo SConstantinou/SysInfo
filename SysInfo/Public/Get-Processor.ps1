b48186dc26fcd36ce694178621c68a7a85ae8df8
Stephanos Constantinou <35262217+SConstantinou@users.noreply.github.com>
2018-10-24 13:16:26 +0300

    Added Help Info

--- SysInfo/Public/Get-Processor.ps1
+++ SysInfo/Public/Get-Processor.ps1
@@ -1,4 +1,83 @@
 ﻿function Get-Processor {
+<#
+.SYNOPSIS
+
+Gets the information about a device that can interpret a sequence
+of instructions on a computer running on a Windows operating system.
+
+.DESCRIPTION
+
+Gets the information about a device that can interpret a sequence
+of instructions on a computer running on a Windows operating system
+and converts all codes in results into human readable format.
+
+.PARAMETER ComputerName
+
+Specifies the computer names or IP Addresses of the systems that
+we want to get the information from.
+
+.INPUTS
+
+System.Array. Get-Processor can accept a string value to
+determine the ComputerName parameter.
+
+.OUTPUTS
+
+System.Object. Get-Processor returns an object containing
+all the information that has been retrieved.
+
+.EXAMPLE
+
+PS C:\> Get-Processor
+
+.EXAMPLE
+
+PS C:\> Get-Processor -ComputerName Server1
+
+.EXAMPLE
+
+PS C:\> Get-Processor -ComputerName "192.168.0.5"
+
+.EXAMPLE
+
+PS C:\> Get-Processor -ComputerName Server1,Server2,Server3
+
+.EXAMPLE
+
+PS C:\> Get-Processor -ComputerName "192.168.0.5","192.168.0.6","192.168.0.7"
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> Get-Processor -ComputerName $MyServers
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> Get-Processor -ComputerName $MyIPs
+
+.EXAMPLE
+
+PS C:\> $MyServers = Server1,Server2,Server3
+PS C:\> $MyServers | Get-Processor
+
+.EXAMPLE
+
+PS C:\> $MyIPs = "192.168.0.5","192.168.0.6","192.168.0.7"
+PS C:\> $MyIPs | Get-Processor
+
+.EXAMPLE
+
+PS C:\> "Server1" | Get-Processor
+
+.EXAMPLE
+
+PS C:\> "192.168.0.5" | Get-Processor
+
+.LINK
+
+https://www.sconstantinou.com/get-processor
+#>
 
     [cmdletbinding()]
 
